2d090dd84340a15cb3ddf54dfa3df8951c2a083d
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-02-14 10:03:55 -0300

    Use simple address param for claim function

--- src/DssProxyRegistry.sol
+++ src/DssProxyRegistry.sol
@@ -44,9 +44,9 @@ contract DssProxyRegistry {
         isProxy[proxy] = 1;
     }
 
-    function claim(address payable proxy) external {
+    function claim(address proxy) external {
         require(isProxy[proxy] == 1, "DssProxyRegistry/not-proxy-from-this-registry");
-        address owner = DssProxy(proxy).owner();
+        address owner = DssProxy(payable(proxy)).owner();
         require(owner == msg.sender, "DssProxyRegistry/only-owner-can-claim");
         address payable prevProxy = payable(proxies[owner]);
         require(prevProxy == address(0) || DssProxy(prevProxy).owner() != owner, "DssProxyRegistry/proxy-registered-to-owner"); // Not allow new proxy if the user already has one and remains being the owner
