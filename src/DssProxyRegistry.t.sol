feffc7bffda441b744215584a6db39214600c0cd
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-04-15 10:52:45 -0300

    Rename variable in tests

--- src/DssProxyRegistry.t.sol
+++ src/DssProxyRegistry.t.sol
@@ -30,8 +30,8 @@ contract DssProxyTest is DSTest {
 
         // This is a workaround to test that build can be also called if the actual proxy of the user was self destructed
         // as selfdestruct is processed at the end of the tx there is not other way with hevm to test that build works after
-        address payable proxyAddr = registry.build(address(this));
-        DssProxy(proxyAddr).execute(address(new Destruct()), abi.encodeWithSignature("destruct()"));
+        address payable proxy = registry.build(address(this));
+        DssProxy(proxy).execute(address(new Destruct()), abi.encodeWithSignature("destruct()"));
     }
 
     function testProxyCreation() public {
@@ -39,10 +39,10 @@ contract DssProxyTest is DSTest {
 
         assertEq(registry.proxies(address(usr)), address(0));
         assertEq(registry.seed(address(usr)), 0);
-        address payable proxyAddr = registry.build(address(usr));
+        address payable proxy = registry.build(address(usr));
         assertEq(registry.seed(address(usr)), 1);
-        assertEq(registry.proxies(address(usr)), proxyAddr);
-        assertEq(DssProxy(proxyAddr).owner(), address(usr));
+        assertEq(registry.proxies(address(usr)), proxy);
+        assertEq(DssProxy(proxy).owner(), address(usr));
     }
 
     function testProxyCreationAfterSelfDestruct() public {
@@ -58,53 +58,53 @@ contract DssProxyTest is DSTest {
     function testProxyClaimAndCreationAfterTransfer() public {
         Usr usr1 = new Usr();
         Usr usr2 = new Usr();
-        address payable proxyAddr = registry.build(address(usr1));
-        assertTrue(proxyAddr != address(0));
+        address payable proxy = registry.build(address(usr1));
+        assertTrue(proxy != address(0));
         assertEq(registry.seed(address(usr1)), 1);
         assertEq(registry.seed(address(usr2)), 0);
-        assertEq(DssProxy(proxyAddr).owner(), address(usr1));
+        assertEq(DssProxy(proxy).owner(), address(usr1));
 
         // Registry set up correctly before transfer
-        assertEq(registry.proxies(address(usr1)), proxyAddr);
+        assertEq(registry.proxies(address(usr1)), proxy);
         assertEq(registry.proxies(address(usr2)), address(0));
 
-        usr1.transferProxy(DssProxy(proxyAddr), address(usr2));
-        assertEq(DssProxy(proxyAddr).owner(), address(usr2));
+        usr1.transferProxy(DssProxy(proxy), address(usr2));
+        assertEq(DssProxy(proxy).owner(), address(usr2));
 
         // Registry now out of date
-        assertEq(registry.proxies(address(usr1)), proxyAddr);
+        assertEq(registry.proxies(address(usr1)), proxy);
         assertEq(registry.proxies(address(usr2)), address(0));
 
-        usr2.claimProxy(registry, proxyAddr);
+        usr2.claimProxy(registry, proxy);
 
         // Registry now reports correctly for usr2 but still shows the old one for usr1
         assertEq(registry.seed(address(usr1)), 1);
         assertEq(registry.seed(address(usr2)), 0);
-        assertEq(registry.proxies(address(usr1)), proxyAddr);
-        assertEq(registry.proxies(address(usr2)), proxyAddr);
+        assertEq(registry.proxies(address(usr1)), proxy);
+        assertEq(registry.proxies(address(usr2)), proxy);
 
         // Can build for usr1 now
-        address payable newProxyAddr = registry.build(address(usr1));
-        assertEq(DssProxy(newProxyAddr).owner(), address(usr1));
-        assertEq(DssProxy(proxyAddr).owner(), address(usr2));
+        address payable proxy2 = registry.build(address(usr1));
+        assertEq(DssProxy(proxy2).owner(), address(usr1));
+        assertEq(DssProxy(proxy).owner(), address(usr2));
 
         // Registry now reports correctly for usr1
         assertEq(registry.seed(address(usr1)), 2);
         assertEq(registry.seed(address(usr2)), 0);
-        assertEq(registry.proxies(address(usr1)), newProxyAddr);
-        assertEq(registry.proxies(address(usr2)), proxyAddr);
+        assertEq(registry.proxies(address(usr1)), proxy2);
+        assertEq(registry.proxies(address(usr2)), proxy);
     }
 
     function testClaimOtherProxyOwned() public {
         Usr usr1 = new Usr();
-        address payable proxyAddr1 = registry.build(address(usr1));
-        assertEq(registry.proxies(address(usr1)), proxyAddr1);
-        address payable proxyAddr2 = registry.build(address(this));
-        DssProxy(proxyAddr2).setOwner(address(usr1));
-        usr1.claimProxy(registry, proxyAddr2);
-        assertEq(registry.proxies(address(usr1)), proxyAddr2);
-        usr1.claimProxy(registry, proxyAddr1);
-        assertEq(registry.proxies(address(usr1)), proxyAddr1);
+        address payable proxy = registry.build(address(usr1));
+        assertEq(registry.proxies(address(usr1)), proxy);
+        address payable proxy2 = registry.build(address(this));
+        DssProxy(proxy2).setOwner(address(usr1));
+        usr1.claimProxy(registry, proxy2);
+        assertEq(registry.proxies(address(usr1)), proxy2);
+        usr1.claimProxy(registry, proxy);
+        assertEq(registry.proxies(address(usr1)), proxy);
     }
 
     function testFailClaimNotAProxy() public {
@@ -113,8 +113,8 @@ contract DssProxyTest is DSTest {
 
     function testFailClaimNotOwnedProxy() public {
         Usr usr1 = new Usr();
-        address payable proxyAddr = registry.build(address(this));
-        DssProxy(proxyAddr).setOwner(address(123));
-        usr1.claimProxy(registry, proxyAddr);
+        address payable proxy = registry.build(address(this));
+        DssProxy(proxy).setOwner(address(123));
+        usr1.claimProxy(registry, proxy);
     }
 }
