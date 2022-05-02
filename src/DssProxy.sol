d15a5e6e9cf13e17ad400657956e1035227bc077
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2022-05-02 12:31:58 -0300

    Fix copyright

--- src/DssProxy.sol
+++ src/DssProxy.sol
@@ -1,4 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// Copyright (C) 2017 DappHub, LLC
 // Copyright (C) 2022 Dai Foundation
 //
 // This program is free software: you can redistribute it and/or modify
