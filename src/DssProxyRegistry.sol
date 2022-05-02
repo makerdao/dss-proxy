857d410c75f2031c87bd9d83aaef7ad9f966c4f5
nikolai <mail@nikolai.fyi>
2022-05-02 10:00:57 -0400

    fix a potential issue for you in the future

--- src/DssProxyRegistry.sol
+++ src/DssProxyRegistry.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (C) 2022 Dai Foundation
+// Copyright (C) 2017  DappHub, LLC
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
--- src/DssProxyRegistry.t.sol
+++ src/DssProxyRegistry.t.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2017  DappHub, LLC
 
 pragma solidity ^0.8.13;
 
